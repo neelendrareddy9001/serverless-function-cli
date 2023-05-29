f1f8461aad49dba4183f5238bb8cd698b3d81492
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-29 20:56:20 +0200

    first-commit

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+const getRandomDadJoke = async () => {
+    // return "Do you want a brief explanation of what an acorn is? In a nutshell, it's an oak tree.";
+    const url = "https://icanhazdadjoke.com/";
+    const jokeStream = await fetch(url, {
+        headers : {
+            Accept: "application/json"
+        }
+    });
+    const jsonJoke = await jokeStream.json();
+    const joke = jsonJoke.joke;
+    return joke;
+};
+
+const displayJoke = (joke) => {
+    const h1 = document.querySelector("h1");
+    h1.textContent = joke;
+};
+
+const rereshJoke = async () => {
+    const joke = await getRandomDadJoke();
+    displayJoke(joke);
+};
+
+//load list oj jokes
+rereshJoke();
+
+setInterval(rereshJoke, 10000);
