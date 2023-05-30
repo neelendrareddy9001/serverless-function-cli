889c9c1250cca754d4f8d2947f753c3046cc9d80
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-30 22:24:28 +0200

    Added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,31 +1,28 @@
 const getRandomDadJoke = async () => {
-    // return "Do you want a brief explanation of what an acorn is? In a nutshell, it's an oak tree.";
-    // const url = "https://icanhazdadjoke.com/";
-    // const jokeStream = await fetch(url, {
-    //     headers : {
-    //         Accept: "application/json"
-    //     }
-    // });
-
-    
+    /* const url = "https://icanhazdadjoke.com/";
+    const jokeStream = await fetch(url, {
+      headers: {
+        Accept: "application/json"
+      }
+    }); */
     const url = "/.netlify/functions/jokes";
     const jokeStream = await fetch(url);
     const jsonJoke = await jokeStream.json();
     const joke = jsonJoke.joke;
     return joke;
-};
-
-const displayJoke = (joke) => {
+  };
+  
+  const displayJoke = (joke) => {
     const h1 = document.querySelector("h1");
     h1.textContent = joke;
-};
-
-const rereshJoke = async () => {
+  };
+  
+  const refreshJoke = async () => {
     const joke = await getRandomDadJoke();
     displayJoke(joke);
-};
-
-//load list oj jokes
-rereshJoke();
-
-setInterval(rereshJoke, 10000);
+  };
+  
+  // load 1st joke
+  refreshJoke();
+  
+  setInterval(refreshJoke, 10000);
